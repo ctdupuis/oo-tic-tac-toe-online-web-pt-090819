d5c0ecf22ab2cb3c8f4ffa5269615bd91837c2cd
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-29 00:41:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,10 +6,10 @@ class TicTacToe
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   
   def display_board
-    puts "X | X | X"
+    puts "X | #{@board[1]} | #{@board[2]}"
     puts "-----------"
-    puts "X | O | O"
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts "-----------"
-    puts "X | O | O"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 end
